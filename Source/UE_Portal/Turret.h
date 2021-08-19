1526f1be639aca4550eaa0ccae71204e25d3375c
DESKTOP-H0JLFMK\Glacier's Desktop <jwkorange@gmail.com>
2021-08-19 14:43:51 +0900

    [Add] Turret Related Contents & Scripts

---
+++ Source/UE_Portal/Turret.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Interactable.h"
+#include "GameFramework/Actor.h"
+#include "Turret.generated.h"
+
+UCLASS()
+class UE_PORTAL_API ATurret : public AInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATurret();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
